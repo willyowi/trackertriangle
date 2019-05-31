3f6cfd524797d3f2d6f225b4debb77ebea6db543
willyowi <wilsonowino1@gmail.com>
2019-05-31 17:26:58 +0300

    scripts.js changes

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,24 +1,28 @@
 function track(){
-var numOne=document.getElementById("one").value;
-var numTwo=document.getElementById("two").value;
-var numThree=document.getElementById("three").value;
-var track=document.getElementById("four")
-
-one=parseInt(numOne)
-two=parseInt(numTwo)
-three=parseInt(numThree)
-
+var one=parseFloat(document.getElementById("one").value);
+var two=parseFloat(document.getElementById("two").value);
+var three=parseFloat(document.getElementById("three").value);
+var track= document.getElementById("four")
 
 if (numOne===numTwo && numOne===numThree && numTwo===numThree) {
-alert("equilatoral triangle")
+alert("equilatoral triangle");
 }
 else if(numOne===numTwo || numOne===numThree || numTwo===numThree0){
-alert("isosceles triangle")
+alert("isosceles triangle");
 }
-else if (numOne!=numTWo && numOne!=numThree && numThree!=numTwo || (numOne+numTWo)!>=numThree || (numOne+numThree)!>=numTWo){
-  alert ("scalene triangle")
+else if (numOne!==numTWo && numOne!==numThree && numThree!==numTwo && numOne+numTWo>numThree && (numTwo+numThree)!>=numOne){
+  alert ("scalene triangle");
+}
+else if (numOne+numTwo <=numThree){
+  alert("not a triangle");
+}
+else if (numOne+numThree <=numTwo ){
+    alert("not a triangle");
+}
+else if (numTwo+numThree <=numOne){
+    alert("not a triangle");
 }
 else {
-  alert(its not a triangle)
+    alert("not a number");
 }
 }
