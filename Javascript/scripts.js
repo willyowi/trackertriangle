b7b09046b41c9debcde3de37d0e6cbeebc2fffc2
willyowi <wilsonowino1@gmail.com>
2019-05-31 15:53:48 +0300

    track button changed

---
+++ Javascript/scripts.js
@@ -0,0 +1,19 @@
+var numOne=document.getElementById("one").value;
+var numTwo=document.getElementById("two").value;
+var numThree=document.getElementById("three").value;
+var track=document.getElementById("four")
+
+var scalene=function(numOne,numTwo,numThree){
+return numOne+numTwo,numOne+numThree,numTwo+numThree
+}
+function track(){
+if (numOne===numTwo && numOne===numThree && numTwo===numThree) {
+alert("equilatoral triangle")
+}
+else if(numOne===numTwo || numOne===numThree || numTwo===numThree0){
+alert("isosceles triangle")
+}
+else if (numOne!=numTWo && numOne!=numThree && numThree!=numTwo || (numOne+numTWo)!>=numThree || (numOne+numThree)!>=numTWo){
+  alert ("scalene triangle")
+}
+}
