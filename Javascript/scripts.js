9d4f80bcae1112a72a92966ef62febd8e5c9c93f
willyowi <wilsonowino1@gmail.com>
2019-05-31 16:47:56 +0300

    add changes in scripts

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,12 +1,14 @@
+function track(){
 var numOne=document.getElementById("one").value;
 var numTwo=document.getElementById("two").value;
 var numThree=document.getElementById("three").value;
 var track=document.getElementById("four")
 
-var scalene=function(numOne,numTwo,numThree){
-return numOne+numTwo,numOne+numThree,numTwo+numThree
-}
-function track(){
+one=parseInt(numOne)
+two=parseInt(numTwo)
+three=parseInt(numThree)
+
+
 if (numOne===numTwo && numOne===numThree && numTwo===numThree) {
 alert("equilatoral triangle")
 }
@@ -15,5 +17,8 @@ alert("isosceles triangle")
 }
 else if (numOne!=numTWo && numOne!=numThree && numThree!=numTwo || (numOne+numTWo)!>=numThree || (numOne+numThree)!>=numTWo){
   alert ("scalene triangle")
+}
+else {
+  alert(its not a triangle)
 }
 }
