0ee284e9ef28ded5af40fd806420b291f8d6c4d6
willyowi <wilsonowino1@gmail.com>
2019-06-03 04:21:59 +0300

    add all chnges for new look

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,21 +1,19 @@
-
-function submit(){
- var sideA=document.getElementById("sideA").value;
- var sideB=document.getElementById("sideB").value;
- var sideC=document.getElementById("sideC").value;
-
- if(sideA==sideB && sideB==sideC){
- alert("equilateral");
- }
- else if (sideA==sideB || sideA==sideC || sideB==sideC){
- alert("isosceles");
- }
- else if ((sideA+sideB <sideC) && (sideA+sideC <sideB) && (sideB+sideC <sideA)){
- alert("not a triangle");
- }
- else if(sideA!=sideB && sideB!=sideC && sideA!=sideC) {
- alert("scalene");
- }
-
-
+function tracker(){
+var side1 = parseFloat(prompt("Enter 1st Number"));
+var side2 = parseFloat(prompt("Enter 2nd Number"));
+var side3 = parseFloat(prompt("Enter 3rd Number"));
+var side = function (a,b,c) {
+return a + b > c && b + c > a && c + a > b
 }
+if(side(side1,side2,side3)&& side1==side2 && side2==side3) {
+alert("Equilateral Triangle")
+}
+else if(side(side1,side2,side3)&& side1==side2 || side2==side3 || side3==side1) {
+alert("Isosceles Triangle")
+}
+else if (side(side1,side2,side3)&& side1!==side2!==side3) {
+alert("Scalene Triangle")
+}
+else {
+alert("Not Triangle!")
+}};
